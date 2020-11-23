63c625d16f70b35e3ab588663c18fceaf40e4373
JPhamNguyen <jphamnguyen@scu.edu>
2020-11-22 22:19:45 -0800

    added basic DASH stream filtering

--- process_video.py
+++ process_video.py
@@ -1,2 +1,7 @@
-if __name__ == "__main__":
-    print("Running process video script")
+#if __name__ == "__main__":
+ #   print("Running process video script")
+
+
+class video(object):
+    #here we initialize the video object for analysis
+    print("accessing the video object for video processing")

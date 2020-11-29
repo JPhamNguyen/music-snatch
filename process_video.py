d58fc7e2223db2af7f9ba7d49186a546d63c50c5
JPhamNguyen <jphamnguyen@scu.edu>
2020-11-28 21:03:07 -0800

    adding frame_capture

--- process_video.py
+++ process_video.py
@@ -18,6 +18,38 @@ def store_video(vid):
         print("create a new directory to store YouTube videos + files")
         os.mkdir('stored_files')
 
-    print("About to download")
-    stream.download('stored_files')
-    print("Done with downloading")
+    # make sure the same youtube video isn't being downloaded (causes infinite download)
+    # if find(file.title, os.path.join(os.getcwd(), 'stored_files')) is True:
+    #    sys.exit('This file has already been downloaded')
+    # else:
+
+    stream.download('stored_files'),
+    print("Finished downloading video into directory")
+
+
+def frame_capture(path):
+    # Path to video file
+    vidObj = cv2.VideoCapture(path)
+
+    # Used as counter variable
+    count = 0
+
+    # checks whether frames were extracted
+    success = 1
+
+    while success:
+        # vidObj object calls read
+        # function extract frames
+        success, image = vidObj.read()
+
+        # Saves the frames with frame-count
+        # cv2.imwrite("frame%d.jpg" % count, image)
+
+        count += 1
+
+
+def find(name, path):
+    # helper function to identify if a video has already been downloaded into directory
+    for root, dirs, files in os.walk(path):
+        if name in files:
+            return True

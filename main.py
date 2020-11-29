e0b6c22e90ddaeb0af18fcd3cdb6b58f9908ad6f
JPhamNguyen <jphamnguyen@scu.edu>
2020-11-28 21:02:47 -0800

    modularized covid

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ import os
 
 
 if __name__ == '__main__':
-    # print("Executing driver function")
     assert len(sys.argv) == 2, "First argument should be the YouTube link containing the music " \
                                "video with the embedded music sheets"
+    # Download the video into a directory
     video = process_video.store_video(sys.argv[1])
-    print("Current program has ended")
+
+    path_of_file = os.path.exists(os.path.join(os.getcwd(), 'stored_files'))
+    print(path_of_file)
+    process_video.frame_capture(path_of_file)
+
+    # after video has been processed, store the pdf into another directory
+

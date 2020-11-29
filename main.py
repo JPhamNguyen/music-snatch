cb7590a44d6db9323f3b4b59e0699456b792cc18
JPhamNguyen <jphamnguyen@scu.edu>
2020-11-28 20:04:57 -0800

    modularized driver code

--- main.py
+++ main.py
@@ -1,25 +1,11 @@
 import process_video
 import sys
 import os
-from pytube import YouTube
+
 
 if __name__ == '__main__':
     # print("Executing driver function")
     assert len(sys.argv) == 2, "First argument should be the YouTube link containing the music " \
                                "video with the embedded music sheets"
-    video = YouTube(sys.argv[1])
-
-    print(video.title)
-    # filter for a stream with the highest quality possible for
-    stream = video.streams.filter(res='1080p', mime_type='video/mp4').first()
-    print(stream)
-
-
-    # check for existing directories for storing files + images
-    print(os.getcwd())
-
-    if not os.path.exists(os.path.join(os.getcwd(), 'stored_files')):
-        print("create a new directory to store YouTube videos + files")
-        os.mkdir('stored_files')
-
-    stream.download('stored_files')
+    video = process_video.store_video(sys.argv[1])
+    print("Current program has ended")
--- process_video.py
+++ process_video.py
@@ -1,7 +1,23 @@
-#if __name__ == "__main__":
- #   print("Running process video script")
+import cv2
+from pytube import YouTube
+import sys
+import os
 
 
-class video(object):
-    #here we initialize the video object for analysis
-    print("accessing the video object for video processing")
+def store_video(vid):
+    file = YouTube(vid)
+    print(file.title)
+    # filter for a stream with 1080p and a mp4 format
+    stream = file.streams.filter(res='1080p', mime_type='video/mp4').first()
+    print(stream)
+
+    # check for existing directories for storing files + images
+    print(os.getcwd())
+
+    if not os.path.exists(os.path.join(os.getcwd(), 'stored_files')):
+        print("create a new directory to store YouTube videos + files")
+        os.mkdir('stored_files')
+
+    print("About to download")
+    stream.download('stored_files')
+    print("Done with downloading")

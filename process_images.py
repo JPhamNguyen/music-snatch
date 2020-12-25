85fe915ec63b37dbfa1b44ec28e30363e300267b
JPhamNguyen <jphamnguyen@scu.edu>
2020-12-24 22:07:10 -0800

    Included MSE, SSIM, starting shape detection

--- process_images.py
+++ process_images.py
@@ -5,24 +5,31 @@ import shutil
 import os
 import cv2
 import numpy as np
-
-def filterFrames(video):
-    print("Entering filterFrames function")
-
-    # for loop to convert images to greyscale
-    # directory = os.listdir(os.getcwd() + '/stored_files/data/')
-    directory = os.listdir('data')
-
-    # for image in os.listdir("data/"):
-    for image in directory:
-        img = cv2.imread(image)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    # Employ rectangular border detection
+from skimage.metrics import structural_similarity as ssim
 
 
+def isolateSheetMusic(frame):
+    # uses rectangular border detection for sheet music
+    blurred = cv2.GaussianBlur(frame, (5, 5), 0)
+    thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
+    cv2.imshow('blurred', blurred)
+    cv2.imshow('threshed', thresh)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
+def compareFrames(frame1, frame2):
+    m = mse(frame1, frame2)
+    s = ssim(frame1, frame2, multichannel=True)
+    print("m: " + str(m))
+    print("s: " + str(s))
+    # need to include a threshold value in order to properly filter images
+
+def mse(imageA, imageB):
+    # Mean Squared Error - the lower the error, the more similar the images are
+    err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
+    err /= float(imageA.shape[0] * imageA.shape[1])
+    return err
 
 
 def createSheetMusic():
--- process_video.py
+++ process_video.py
@@ -2,9 +2,12 @@ import cv2
 from pytube import YouTube
 import sys
 import os
+import process_images
+import numpy as np
 
 
 def store_video(vid):
+    # if not already downloaded, store video into a specific directory
     file = YouTube(vid)
     # filter for a stream with 1080p and a mp4 format
     stream = file.streams.filter(res='1080p', mime_type='video/mp4').first()
@@ -22,7 +25,7 @@ def store_video(vid):
 
 
 def frame_capture(video):
-    # Path to video file
+    # Splits video into frames to be analyzed later
     path = os.getcwd() + '/stored_files/' + str(video)
     print(video)
     cap = cv2.VideoCapture(path)
@@ -32,24 +35,48 @@ def frame_capture(video):
     frames_data = 'data/' + video
     print(frames_data)
     try:
-        # if not os.path.exists('data/' + video):
         if not os.path.exists(frames_data):
             os.makedirs('data')
     except OSError:
         print('Error: Creating directory of data')
 
+    # iterate over video's frames and store them as greyscale images
     currentFrame = 0
+    previousFrame = ''
     # while True:
     while currentFrame != length:
+        # read image
         ret, frame = cap.read()
 
-        name = './data/frame' + str(currentFrame) + '.jpg'
-        print('Creating...' + name)
-        cv2.imwrite(name, frame)
-        currentFrame += 1
+        # isolate sheet music from video frame
+        frame = process_images.isolateSheetMusic(frame)
+
+        if currentFrame == 0:
+            name = './data/frame' + str(currentFrame) + '.jpg'
+            print('Creating...' + name)
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            cv2.imwrite(name, frame)
+            currentFrame += 1
+            previousFrame = frame
+        # else:
+            # bool value to describe if two images are duplicates
+            # duplicate = process_images.compareFrames(frame, previousFrame)
+            # if duplicate is True:
+                # pass
+            # else:
+                # name = './data/frame' + str(currentFrame) + '.jpg'
+                # print('Creating...' + name)
+                # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                # cv2.imwrite(name, frame)
+                # currentFrame += 1
+                # previousFrame = frame
 
     cap.release()
     cv2.destroyAllWindows()
+
+
+def processMetadata(video):
+    print("intended to return important timestamps and data")
 
 
 def find(name, path):

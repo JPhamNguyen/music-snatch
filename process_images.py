2364a6c98293e6082a7240b33c69c4c3164bd890
JPhamNguyen <jphamnguyen@scu.edu>
2020-12-25 18:41:16 -0800

    Implementing contour detection

--- process_images.py
+++ process_images.py
@@ -6,14 +6,20 @@ import os
 import cv2
 import numpy as np
 from skimage.metrics import structural_similarity as ssim
+import imutils
 
 
 def isolateSheetMusic(frame):
     # uses rectangular border detection for sheet music
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     blurred = cv2.GaussianBlur(frame, (5, 5), 0)
     thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
     cv2.imshow('blurred', blurred)
     cv2.imshow('threshed', thresh)
+
+    contours = cv2.findContours(thresh.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours = imutils.grab_contours(contours)
+
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
@@ -24,6 +30,7 @@ def compareFrames(frame1, frame2):
     print("m: " + str(m))
     print("s: " + str(s))
     # need to include a threshold value in order to properly filter images
+
 
 def mse(imageA, imageB):
     # Mean Squared Error - the lower the error, the more similar the images are
